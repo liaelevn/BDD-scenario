feb297d7e79518ef8bad9cc8b6ba9242cf9257c6
liaelevn <112964705+liaelevn@users.noreply.github.com>
2023-03-03 13:38:04 +0000

    Add files via upload

---
+++ Harrods BDD Scenario.py
@@ -0,0 +1,12 @@
+# Harrods Upgrade membership
+
+
+# Feature: Black Tier member upgrade
+
+# As a Harrods member I want to upgrade my Gold Tier membership,
+# so I'm able to become a Black Tiers member for private shopping at Harrods
+
+# Scenario
+# Given that I am a Gold Tier Harrods customer,
+# When I click on <Join Now> button and enter my details in all empty fields
+# Then, the account should upgrade to Black Tiers account successfully

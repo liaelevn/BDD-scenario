1b8c3bebd7a36a4102e23d9e5e982fccb85f9a58
liaelevn <112964705+liaelevn@users.noreply.github.com>
2023-03-03 13:27:16 +0000

    Add files via upload

---
+++ Tesco Login BDD Scenario.py
@@ -0,0 +1,15 @@
+# Tesco Clubcard Login fail
+
+
+# Feature: Clubcard login unsuccessful
+
+# As a Clubcard user, I want to login into my account
+# so that I am able to scan my clubcard
+
+# Scenario:
+# Given I have logged in,
+# When I enter <url> on search bar
+# When I enter <Username>
+# Then enter <password> and click <enter>
+# Then I should be able to login successfully.
+

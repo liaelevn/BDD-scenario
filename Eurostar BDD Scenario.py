ef6943df520b6df0f0890611838e3c4cda58eca4
liaelevn <112964705+liaelevn@users.noreply.github.com>
2023-03-03 13:37:42 +0000

    Add files via upload

---
+++ Eurostar BDD Scenario.py
@@ -0,0 +1,15 @@
+# Eurostar sale ticket for user
+
+
+# Feature: Spring Sale ticket
+
+# Eurostar user wants to purchase £ 39 Spring sale ticket for Paris
+# so that they travel to Paris without paying the original price
+
+# Scenario
+# Given the eurostar cart is empty,
+# When I navigate the Eurostar homepage and click <buy tickets>,
+# The cart should have one arrival ticket to Paris and one return ticket back to London
+# Then the £ 39 ticket should be purchased successfully
+
+
